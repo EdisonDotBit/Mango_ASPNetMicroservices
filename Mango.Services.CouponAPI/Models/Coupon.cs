6fd06532a379e7356fc591bcceecb269815eaac6
Edison Lati Jr. <edisonlatijr.h2software@gmail.com>
2026-05-12 13:23:07 +0800

    Add Coupon.cs and CouponDto.cs
    This serves as a entity model and DTO model for the application to use to process or return data.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CoouponId { get; set; }
+        public string CouponCode { get; set; }
+        public int DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CoouponId { get; set; }
+        public string CouponCode { get; set; }
+        public int DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
