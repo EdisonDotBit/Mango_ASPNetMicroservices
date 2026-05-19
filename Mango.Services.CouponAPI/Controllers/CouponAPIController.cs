0cdb8ab72f2407aa7f3be84bd50b41b5f058670f
Edison Lati Jr. <junior.lati@gmail.com>
2026-05-19 14:52:58 +0800

    Add CouponAPIController and Update Project Configuration
    Added `CouponAPIController` with endpoints to retrieve all coupons and a specific coupon by ID. Integrated dependency injection for `AppDbContext` and included necessary `using` directives.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> couponList = _db.Coupons.ToList();
+                return couponList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(x => x.CouponId == id);
+                return coupon;
+            }
+            catch (Exception ex) 
+            {
+
+            }
+            return null;
+        }
+    }
+}
