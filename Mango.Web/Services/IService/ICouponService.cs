c525d62929c122f2c4a8b87f63db78d256c319a3
Edison Lati Jr. <junior.lati@gmail.com>
2026-07-08 17:58:14 +0800

    Add CouponDto model and ICouponService interface
    Introduced CouponDto for coupon data representation. Added ICouponService interface with async CRUD and retrieval methods for coupons, returning ResponseDto.

---
+++ Mango.Web/Services/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> GetCouponByCodeAsync(string couponCode);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto token);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+
+    }
+}
